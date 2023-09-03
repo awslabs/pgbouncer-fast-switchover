27eca409a329d2a3a056138bcb077f29c341a5d4
yahavb <yahavb@amazon.com>
2023-09-03 11:59:15 -0700

    pgb-multi-arch-ci

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 PGB_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$PGB_REPO:$PGB_TAG
-export BASE_IMAGE="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$PGB_REPO:$PGB_TAG"
+export BASE_IMAGE="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$BASE_TAG"
 cat Dockerfile.template | envsubst > Dockerfile
 cat Dockerfile
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $PGB_IMAGE
