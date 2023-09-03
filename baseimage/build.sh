6798c7008d80af684d0af5f8531aa1e2e4528ea8
yahavb <yahavb@amazon.com>
2023-09-02 22:04:41 -0700

    docker ci w/python37

--- baseimage/build.sh
+++ baseimage/build.sh
@@ -3,7 +3,9 @@ BASE_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$BASE_IM
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $BASE_IMAGE
 if [ "$ARCH" = "arm64" ]; then
   AWSCLIARCH="aarch64"
+fi
 if [ "$ARCH" = "amd64" ]; then
   AWSCLIARCH="x86_64"
+fi
 docker build --build-arg PANDOC_VER=$PANDOC_VER --build-arg ARCH=$ARCH --build-arg AWSCLIARCH=$AWSCLIARCH -t $BASE_IMAGE .
 docker push $BASE_IMAGE
